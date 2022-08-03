a322634a7a93e6a085ffd495e031e2cb26719132
Martin Finkel <me@martinfinkel.com>
2022-08-03 20:33:59 +0700

    vlc-debian-llvm-mingw: bump to llvm 13

--- vlc-debian-llvm-mingw/build-llvm.sh
+++ vlc-debian-llvm-mingw/build-llvm.sh
@@ -16,7 +16,7 @@
 
 set -e
 
-: ${LLVM_VERSION:=llvmorg-12.0.0}
+: ${LLVM_VERSION:=llvmorg-13.0.0}
 ASSERTS=OFF
 unset HOST
 BUILDDIR="build"
@@ -77,11 +77,6 @@ if [ -n "$SYNC" ] || [ -n "$CHECKOUT" ]; then
     cd llvm-project
     [ -z "$SYNC" ] || git fetch
     git checkout $LLVM_VERSION
-    if [ "$LLVM_VERSION" = "llvmorg-12.0.0" ]; then
-        # The bundled patches for std::filesystem apply on the 12.0.0 release,
-        # but don't try to apply them if a different version was requested.
-        git am -3 --keep-non-patch ../patches/llvm-project/*.patch
-    fi
     cd ..
 fi
 
